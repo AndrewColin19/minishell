292e03f0ed3be715289a3a2a34466acf8dc98682
andrew <andrew19@live.fr>
2021-12-16 16:05:21 +0100

    echo ok

--- srcs/fct.c
+++ srcs/fct.c
@@ -6,33 +6,11 @@
 /*   By: acolin <acolin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 15:25:03 by andrew            #+#    #+#             */
-/*   Updated: 2021/12/16 16:04:24 by acolin           ###   ########.fr       */
+/*   Updated: 2021/12/16 16:05:18 by acolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-
-/*void	cmd_echo(int fd, char *cmd)
-{
-	int	ln;
-
-	ln = 1;
-	if (cmd[5] == '-' && cmd[6] == 'n')
-	{
-		ln = 0;
-		cmd += 8;
-	}
-	while (*cmd && *cmd == ' ')
-		cmd += 1;
-	if (ln && *cmd != '\0')
-	{
-		putstr(fd, cmd + 5);
-		write(fd, "\n", 1);
-	}
-	else if (*cmd != '\0')
-		putstr(fd, cmd + 8);
-	add_history(cmd);
-}*/
 
 void	cmd_echo(int fd, char *cmd)
 {
