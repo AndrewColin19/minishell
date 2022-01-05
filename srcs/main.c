3f6651be1e2a93a5b412c2e2be3c8bd47106c2b6
Loan Matarise <lmataris@c2r6p3.42nice.fr>
2022-01-05 14:32:37 +0100

    Redirection out 100%

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lmataris <lmataris@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 11:01:16 by acolin            #+#    #+#             */
-/*   Updated: 2022/01/05 12:22:57 by lmataris         ###   ########.fr       */
+/*   Updated: 2022/01/05 14:31:32 by lmataris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,66 +26,24 @@ void	select_cmd(char *cmd, int in, int out)
 		cmd_exec(cmd, in, out);
 }
 
-	void	exec_rec(char **cmds, int i, int in)
-	{
-		int 	pipes[2];
-		int		redirect_out;
-
-		pipe(pipes);
-		redirect_out = check_redirection(&cmds[i]);
-		if (cmds[i + 1])
-		{
-			select_cmd(cmds[i], in, pipes[1]);
-			close(pipes[1]);
-			if (redirect_out)
-				write_redirection(pipes[0], redirect_out);
-			exec_rec(cmds, i + 1, pipes[0]);
-		}
-		else
-		{
-			if (redirect_out)
-			{
-				select_cmd(cmds[i], in, pipes[1]);
-				close(pipes[1]);
-				write_redirection(pipes[0], redirect_out);
-			}
-			else
-				select_cmd(cmds[i], in, 1);
-		}
-	}
-
-void	exec(char **cmds)
+void	exec(char **cmds, int i, int in)
 {
-	exec_rec(cmds, 0, 0);
-	/*int		i;
 	int 	pipes[2];
-	int		fd;
-	int		redirect;
+	int		redirect_out;
 
-	i = -1;
-	fd = 0;
-	while (cmds[++i] && cmds[i + 1])
+	pipe(pipes);
+	redirect_out = check_redirection(&cmds[i]);
+	if (cmds[i + 1] || redirect_out)
 	{
-		redirect = check_redirection(&cmds[i]);
-		if (i != 0)
-			fd = pipes[0];
-		pipe(pipes);
-		select_cmd(cmds[i], fd, pipes[1]);
+		select_cmd(cmds[i], in, pipes[1]);
 		close(pipes[1]);
-		if (redirect != NULL)
-			write_redirection(pipes[0], redirect);
-	}
-	redirect = check_redirection(&cmds[i]);
-	if (redirect)
-	{
-		fd = pipes[0];
-		pipe(pipes);
-		select_cmd(cmds[i], fd, pipes[1]);
-		close(pipes[1]);
-		write_redirection(pipes[0], redirect);
+		if (redirect_out)
+			write_redirection(pipes[0], redirect_out);
+		if (cmds[i + 1] )
+			exec(cmds, i + 1, pipes[0]);
 	}
 	else
-		select_cmd(cmds[i], pipes[0], 1);*/
+		select_cmd(cmds[i], in, 1);
 }
 
 int	main(int argc, char *argv[], char **ev)
@@ -106,7 +64,7 @@ int	main(int argc, char *argv[], char **ev)
 		else
 		{
 			export_var(cmds);
-			exec(cmds);
+			exec(cmds, 0, 0);
 		}
 	}
 }
