39f5da2652460ec0482cebc23f127f965ec2c86e
Loan Matarise <lmataris@c2r6p3.42nice.fr>
2022-01-05 12:24:04 +0100

    test

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lmataris <lmataris@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 11:01:16 by acolin            #+#    #+#             */
-/*   Updated: 2022/01/05 11:11:11 by lmataris         ###   ########.fr       */
+/*   Updated: 2022/01/05 12:22:57 by lmataris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,41 @@ void	select_cmd(char *cmd, int in, int out)
 		cmd_exec(cmd, in, out);
 }
 
+	void	exec_rec(char **cmds, int i, int in)
+	{
+		int 	pipes[2];
+		int		redirect_out;
+
+		pipe(pipes);
+		redirect_out = check_redirection(&cmds[i]);
+		if (cmds[i + 1])
+		{
+			select_cmd(cmds[i], in, pipes[1]);
+			close(pipes[1]);
+			if (redirect_out)
+				write_redirection(pipes[0], redirect_out);
+			exec_rec(cmds, i + 1, pipes[0]);
+		}
+		else
+		{
+			if (redirect_out)
+			{
+				select_cmd(cmds[i], in, pipes[1]);
+				close(pipes[1]);
+				write_redirection(pipes[0], redirect_out);
+			}
+			else
+				select_cmd(cmds[i], in, 1);
+		}
+	}
+
 void	exec(char **cmds)
 {
-	int		i;
+	exec_rec(cmds, 0, 0);
+	/*int		i;
 	int 	pipes[2];
 	int		fd;
-	char	*redirect;
+	int		redirect;
 
 	i = -1;
 	fd = 0;
@@ -47,7 +76,7 @@ void	exec(char **cmds)
 			write_redirection(pipes[0], redirect);
 	}
 	redirect = check_redirection(&cmds[i]);
-	if (redirect != NULL)
+	if (redirect)
 	{
 		fd = pipes[0];
 		pipe(pipes);
@@ -56,7 +85,7 @@ void	exec(char **cmds)
 		write_redirection(pipes[0], redirect);
 	}
 	else
-		select_cmd(cmds[i], pipes[0], 1);
+		select_cmd(cmds[i], pipes[0], 1);*/
 }
 
 int	main(int argc, char *argv[], char **ev)
